84d12263f50285084710510f2cf9ad913ea8d649
Bryan Ayertey <bryanbest123@gmail.com>
2023-12-07 10:40:48 +0000

    Correcting to rubric

--- Post Setup Queries 3.sql
+++ Post Setup Queries 3.sql
@@ -1,13 +1,7 @@
 use littlelemondb;
 
-delimiter //
 create procedure addBooking(in bid int,in bdate date, in tnumber int,in custid int )
-begin
 insert into bookings values(bid,bdate,tnumber,custid);
-select "Booking has been added";
-end //
-
-delimiter ;
 
 call addBooking(7,'2022-11-10',2,3);
 
@@ -23,14 +17,8 @@ delimiter ;
 
 call updatebooking(7,'2022-11-12');
 
-delimiter //
-
 create procedure cancelBooking(in bid int )
-begin
 delete from bookings where bookingid = bid;
-select concat("Booking ", bid," has been cancelled");
-end//
 
-delimiter ;
 
 call cancelbooking(7);
